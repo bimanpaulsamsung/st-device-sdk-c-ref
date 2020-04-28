c6dceedc8dcaa024aa5283860616da5d5fdebded
Biman Paul <biman.paul@samsung.com>
2020-04-28 15:57:59 +0530

    st_switch application for cypress

---
+++ apps/cy8cproto_062_4343w/st_switch/README.md
@@ -0,0 +1,15 @@
+# Mbed OS Smart_Switch Demo
+
+## 1. Introduction
+
+This is an example application installed on the IoT device to connect directly to the SmartThings Cloud Server through mqtt protocol.
+
+## 2. Configuration
+* `mbed compile -m cy8cproto_062_4343w -t GCC_ARM --flash` to compile and flash
+
+## 3. Compiling & Execution
+
+Once all the aforementioned works are done, the application will:
+
+* Do onboarding procedure
+* Control or Monitor the device with SmartThings App.
---
+++ apps/cy8cproto_062_4343w/st_switch/main/device_control.h
@@ -0,0 +1,41 @@
+/* ***************************************************************************
+ *
+ * Copyright 2020 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+#ifndef _DEVICE_CONTROL_H_
+#define _DEVICE_CONTROL_H_
+
+#include "mbed.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define GPIO_OUTPUT_MAINLED LED1
+
+enum main_led_gpio_state
+{
+	MAINLED_GPIO_ON = 1,
+	MAINLED_GPIO_OFF = 0,
+};
+
+void gpio_set_level(PinName gpio_pin, int value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
