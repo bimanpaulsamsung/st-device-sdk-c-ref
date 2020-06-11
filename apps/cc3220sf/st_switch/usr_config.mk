ba6e200c2a9a3e7791b3486e44cc89ab8082154a
hong.ye <52810083+hongye-samsung@users.noreply.github.com>
2020-06-11 16:41:33 +0800

    [Stage] Add new patch for increasing stack size for app_main & add app config
    Signed-off-by: hong.ye <52810083+hongye-samsung@users.noreply.github.com>

---
+++ apps/cc3220sf/st_switch/usr_config.mk
@@ -0,0 +1,7 @@
+########################################
+# user Config
+########################################
+
+CONFIG_STDK_IOT_CORE_SECURITY_BACKEND_SOFTWARE=y
+CONFIG_STDK_IOT_CORE_SECURITY_BACKEND_HARDWARE=
+
