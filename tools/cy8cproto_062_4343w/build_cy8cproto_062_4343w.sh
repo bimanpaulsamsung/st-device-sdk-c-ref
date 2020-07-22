00b5d682d11db3b60b0e32ea4e3e944e2eda3aa1
Biman Paul <biman.paul@samsung.com>
2020-07-22 09:33:04 +0530

    add Include list

--- tools/cy8cproto_062_4343w/build_cy8cproto_062_4343w.sh
+++ tools/cy8cproto_062_4343w/build_cy8cproto_062_4343w.sh
@@ -30,6 +30,32 @@ if [ ! -d ${PROJECT_PATH} ]; then
   exit 1
 fi
 
+
+#Generate .mbedignore
+File=".mbedignore"
+INCLUDE_FILES="MbedIncludeList.txt"
+
+echo "Generating .mbedignore ..."
+index=0
+while read line; do
+        LIST[$index]="$line"
+        index=$(expr $index + 1)
+done < "$PWD/$INCLUDE_FILES"
+
+find iot-core/ -name "*.c" > $File
+find iot-core/ -name "*.cpp" >> $File
+find iot-core/ -name "*.a" >> $File
+find apps/ -name "*.c" >> $File
+find apps/ -name "*.cpp" >> $File
+
+echo "Processing...."
+for i in "${LIST[@]}"
+do
+        sed -i -e "/"${i//\//\\\/}"/d" $File
+done
+echo "Generated .mbedignore"
+
+#Check Options
 if [ "${3}" = "flash" ]; then
   shift 2
   FLASH_OPTION="--$@"
@@ -37,13 +63,13 @@ fi
 
 
 #Genarate Root CA
-cd ${CORE_PATH}
-cat src/include/certs/boilerplate.h > src/iot_root_ca.c
-xxd -i src/certs/${CA_CERT_FILE} >> src/iot_root_ca.c
-sed -i.bak 's/src.*pem/st_root_ca/g' src/iot_root_ca.c
-sed -i.bak 's/unsigned/const unsigned/g' src/iot_root_ca.c
-rm -f src/iot_root_ca.c.bak
-cd ${STDK_PATH}
+#cd ${CORE_PATH}
+#cat src/include/certs/boilerplate.h > src/iot_root_ca.c
+#xxd -i src/certs/${CA_CERT_FILE} >> src/iot_root_ca.c
+#sed -i.bak 's/src.*pem/st_root_ca/g' src/iot_root_ca.c
+#sed -i.bak 's/unsigned/const unsigned/g' src/iot_root_ca.c
+#rm -f src/iot_root_ca.c.bak
+#cd ${STDK_PATH}
 
 #Genarate Object Files from json files
 cd ${PROJECT_PATH}/main
