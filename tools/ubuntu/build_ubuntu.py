cc55cf887bfe709095f93721864e2ddfdf1f05fa
Pooja <pooja.k@partner.samsung.com>
2020-10-30 20:50:46 +0530

    Update python build script for ubuntu

--- tools/ubuntu/build_ubuntu.py
+++ tools/ubuntu/build_ubuntu.py
@@ -2,12 +2,15 @@
 
 import sys
 import os
+import re
 
 BSP_NAME = sys.argv[1]
 APP_NAME = sys.argv[2]
 EXTRA_ARGS = sys.argv[3:]
 
 APP_PATH = os.path.join(os.environ["STDK_REF_PATH"], "apps", BSP_NAME, APP_NAME)
+EXECUTABLE_PATH = os.path.join(APP_PATH, "main", APP_NAME)
+SDKCONFIG = os.path.join(APP_PATH, "sdkconfig")
 
 if not (os.path.exists(APP_PATH)):
     print("Fail to find path.")
@@ -19,6 +22,15 @@ if EXTRA_ARGS:
         MAKE_OPTION = MAKE_OPTION + " " + args
 else:
     MAKE_OPTION = "all"
+
+CFLAGS_CONFIG = ""
+Read_File = open(SDKCONFIG, 'r')
+for lines in Read_File:
+    match = re.search("(\w+=\w+)", lines)
+    if match:
+        CFLAGS_CONFIG = CFLAGS_CONFIG + " -D" + match.group()
+Read_File.close()
+os.environ["CFLAGS_CONFIG"] = CFLAGS_CONFIG
 
 build_cmd = "make" + " " + MAKE_OPTION
 os.chdir(os.environ["STDK_CORE_PATH"])
