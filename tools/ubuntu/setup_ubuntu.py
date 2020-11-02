d03c8f03983365045b564a13a91e69d8bc4d305e
sahil.pai1 <sahil.pai1@samsung.com>
2020-11-02 17:23:04 +0530

    tools/ubuntu: use core path environment variable
    Use STDK_CORE_PATH envirtonment variable instead of local
    variable
    
    Signed-off-by: sahil.pai1 <sahil.pai1@samsung.com>

--- tools/ubuntu/setup_ubuntu.py
+++ tools/ubuntu/setup_ubuntu.py
@@ -4,8 +4,7 @@ import sys
 import os
 
 BSP_NAME=sys.argv[1]
-CORE_PATH = os.path.join(os.environ["STDK_CORE_PATH"])
 
-os.chdir(CORE_PATH)
+os.chdir(os.environ["STDK_CORE_PATH"])
 os.system("git submodule update --init --recursive")
 os.system("git submodule foreach --recursive git reset --hard")
